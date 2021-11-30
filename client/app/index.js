30aa04aca2be0873b755ee1d98e17d0f14cb1739
Abner.Lima <abnerlima.1997@gmail.com>
2021-11-30 05:01:04 -0300

    alert oi

---
+++ client/app/index.js
@@ -0,0 +1,13 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade'),
+]
+
+console.log(campos)
+
+var tbody = document.querySelector('table body')
+
+document.querySelector('.form').addEventListener('submit', (evento) => {
+    alert('oi')
+})
