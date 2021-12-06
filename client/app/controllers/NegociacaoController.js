4ec7f80a458c061d5fac3a3b46a44bfb3831522e
Abner.Lima <abnerlima.1997@gmail.com>
2021-12-06 19:03:19 -0300

    Criando model Negociacoes

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,27 +1,31 @@
 class NegociacaoController {
-
     constructor() {
         let $ = document.querySelector.bind(document)
-    
-        this._inputData = $('#data')
-        this._inputQuantidade = $('#quantidade')
-        this._inputValor = $('#valor')
+        this._inputData = $("#data")
+        this._inputQuantidade = $("#quantidade")
+        this._inputValor = $("#valor")
+        this._negociacoes = new Negociacoes()
     }
 
     adiciona(event) {
         event.preventDefault()
+        this._negociacoes.adiciona(this._criaNegociacao())
+        console.log(this._negociacoes.paraArray())
+        this._limpaFormulario()
+    }
 
-        let negociacao = new Negociacao(
+    _criaNegociacao() {
+        return new Negociacao(
             DataConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         )
-
-        console.log(negociacao.data)
-
-        let diaMesAno = DataConverter.paraTexto(negociacao.data)
-
-        console.log(diaMesAno)
     }
 
+    _limpaFormulario() {
+        this._inputData.value = ''
+        this._inputQuantidade.value = 1
+        this._inputValor.value = 0.0
+        this._inputData.focus()
+    }
 }
--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -9,7 +9,8 @@ class DataConverter {
     }
     
     static paraData(texto) {
-        if (!/^\d(4)-\d(2)-\d(2)$/.test(texto)) throw new Error('A data deve estar no formato aaaa-mm-dd')
+        if (!/^\d{4}-\d{2}-\d{2}$/.test(texto)) 
+            throw new Error('A data deve estar no formato aaaa-mm-dd')
         return new Date(...texto.split('-').map(
             (item, indice) => item - indice % 2
         ))
