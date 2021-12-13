0c906b73b855bd0f3bbdda351324bc8f0f35a1cb
Abner.Lima <abnerlima.1997@gmail.com>
2021-12-13 20:22:32 -0300

    Associando models e vies através da classe Bind

--- client/app/util/ProxyFactory.js
+++ client/app/util/ProxyFactory.js
@@ -1,9 +1,9 @@
-class ProxyFactory{
+class ProxyFactory {
     static create(objeto, props, armadilha) {
         return new Proxy(objeto, {
             get(target, prop, receiver) {
                 if (ProxyFactory._ehFuncao(target[prop]) && props.includes(prop)) {
-                    return function() {
+                    return function () {
                         console.log(`${prop} disparou a armadilha!`)
                         target[prop].apply(target, arguments)
                         armadilha(target)
@@ -14,13 +14,13 @@ class ProxyFactory{
             },
             set(target, prop, value, receiver) {
                 const updated = Reflect.set(target, prop, value)
-                if(props.includes(prop)) armadilha(target)
+                if (props.includes(prop)) armadilha(target)
                 return updated
             }
         })
     }
 
     static _ehFuncao(fn) {
-        return typeof(fn) == typeof(Function)
+        return typeof (fn) == typeof (Function)
     }
 }
