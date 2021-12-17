183a32c50063ce9f7ffd4098d2e2fe92f7e7f264
Abner.Lima <abnerlima.1997@gmail.com>
2021-12-17 04:17:17 -0300

    13.3 Criando Promises

--- client/app/domain/negociacao/NegociacaoService.js
+++ client/app/domain/negociacao/NegociacaoService.js
@@ -1,20 +1,21 @@
 class NegociacaoService {
-    obterNegociacoesDaSemana(cb) {
-        const xhr = new XMLHttpRequest()
-        xhr.open('GET', 'negociacoes/semana')
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState == 4) {
-                if (xhr.status == 200) {
-                    const negociacoes = JSON
-                        .parse(xhr.responseText)
-                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
-                        cb(null, negociacoes)
-                } else {
-                    console.log(xhr.responseText)
-                    cb('Não foi possível obter as negociações da semana', null)
+    obterNegociacoesDaSemana() {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest()
+            xhr.open('GET', 'negociacoes/semana')
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        const negociacoes = JSON
+                            .parse(xhr.responseText)
+                            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                        resolve(negociacoes)
+                    } else {
+                        reject('Não foi possível obter as negociações da semana', null)
+                    }
                 }
             }
-        }
-        xhr.send()
+            xhr.send()
+        })
     }
 }
