d1996d689768206e8f8bd440e4e56a0af5a8c894
Abner.Lima <abnerlima.1997@gmail.com>
2021-12-29 04:51:35 -0300

    15.2 Criando stores

--- client/app/util/ConnectionFactory.js
+++ client/app/util/ConnectionFactory.js
@@ -16,29 +16,42 @@ class ConnectionFactory {
 
             //Triade de eventos para a abertura de uma conexão com IndexDB
 
-            //Itera o array de stores para construí-las
-            stores.forEach(store => {
-
-            })
-
             //O método open abre a conexão passando o nome da Store e sua versão
             const openRequest = indexedDB.open('jscangaceiro', 2)
 
-            //Lógica a ser executada caso a coneão necessite de atualização
+            //Lógica a ser executada caso a coneão necessite ser criada ou de atualização
             openRequest.onupgradeneeded = e => {
-
+                //Se a conexão for bem sucedida, será chamado o método responsável por criar a store
+                ConnectionFactory._createStores(e.target.result)
             }
 
             //Lógica a ser utilizada caso a negociação obtenha sucesso
             openRequest.onsuccess = e => {
-
+                //Se a conexão for bem sucedida, será enviada como retorno através do resolve
+                resolve(e.target.result)
             }
 
             //Lógica a ser utilizada caso a conexão retorne um erro
             openRequest.onerror = e => {
-
+                //Se a conexão obter erro, será enviado como retorno através de reject
+                reject(e.target.result)
             }
 
+        })
+    }
+
+    //Método criado para deixar clara a intenção de criar uma store
+    static _createStores(connection) {
+        stores.forEach(store => {
+
+            //Se já houver uma store com mesmo nome, está será excluida
+            if(connection.objectStoreNames.contains(store))
+                connection.deleteObjectStore(store)
+
+            //Será criada a nova store com as propriedades do objeto options passado como argumento.
+            connection.createObjectStore(store, {
+                autoIncrement: true
+            })
         })
     }
 }
