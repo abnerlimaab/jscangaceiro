971dbe972353fa92dbaaf07f90d3b4d72a0ff324
Abner.Lima <abnerlima.1997@gmail.com>
2021-12-30 15:52:11 -0300

    16.3 Implementando a lógica de inclusão

--- client/app/domain/negociacao/NegociacaoDao.js
+++ client/app/domain/negociacao/NegociacaoDao.js
@@ -7,7 +7,16 @@ class NegociacaoDao {
 
     adiciona(negociacao) {
         return new Promise((resolve, reject) => {
+            const request = this._connection
+                .transaction([this._store], 'readwrite')
+                .objectStore(this._store)
+                .add(negociacao)
 
+            request.onsuccess = e => resolve()
+            request.onerror = e => {
+                console.log(e.target.error)
+                reject('Não foi possível salvar a negociação')
+            }
         })
     }
 
